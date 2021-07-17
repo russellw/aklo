8836f9be06ec50203654da1e8d3821af8a594d76
Russell Wallace <russell.wallace@gmail.com>
2021-07-17 15:09:16 +0100

    terms

---
+++ Loc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+struct Loc
+{
+    public string file;
+    public int line;
+
+    public Loc(string file, int line)
+    {
+        this.file = file;
+        this.line = line;
+    }
+}
---
+++ Term.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Term
+{
+    public Loc loc;
+    public readonly Tag tag;
+    public List<Term> contents;
+
+    public Term(Tag tag)
+    {
+        this.tag = tag;
+    }
+}
